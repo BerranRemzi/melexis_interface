a2bddaa2a26fd8ff5b981fd67bdb076649f74f61
Berran Remzi <berranremzi@gmail.com>
2019-08-18 21:27:18 +0300

    Simplify examle project

--- examples/MLX90363Read/MLX90363Read.ino
+++ examples/MLX90363Read/MLX90363Read.ino
@@ -1,40 +1,24 @@
 #include <SPI.h>
 #include "melexis.h"
 
-MELEXIS test(15);
+#define SS_PIN 10
+
+MELEXIS test(SS_PIN);
 
 void setup()
-{                
-  Serial.begin(38400);  
+{
+  Serial.begin(115200);
 }
 
 void loop()
 {
   delayMicroseconds(2500); //tSSRERE_mod3
-  if (test.poll())
-    Serial.println("No Response");
-    
+  test.poll();
+
   Serial.print("X: ");
-  Serial.println(test.get_x(),DEC);
+  Serial.print(test.get_x(), DEC);
   Serial.print(" Y: ");
-  Serial.println(test.get_y(),DEC);
-
-  /*Serial.print(" orig: ");
-  Serial.println(test.get_eeprom(MELEXIS_EE_3D),BIN);
-  Serial.print("write: ");
-  Serial.println(test.set_eeprom(MELEXIS_EE_3D,1),DEC);
-  Serial.print("  new: ");
-  Serial.println(test.get_eeprom(MELEXIS_EE_3D),BIN);
-
-  while (1)
-  {
-    Serial.print("  done ");
-    delay(1000);
-  }*/
-  /*Serial.print("Diag0: ");
-  Serial.println(test.get_diag_0(),DEC);
-  Serial.print("Diag1: ");
-  Serial.println(test.get_diag_1(),DEC);*/
-  
+  Serial.print(test.get_y(), DEC);
+  Serial.print(" Z: ");
+  Serial.println(test.get_z(), DEC);
 }
-
